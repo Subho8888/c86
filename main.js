bb0d72221b5bf46b4e7300bea0fc24baf1307a95
Subho8888 <87801594+Subho8888@users.noreply.github.com>
2021-08-27 13:45:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+var canvas=new fabric.canvas("myCanvas");
+
+player_x=10;
+player_y=10;
+
+block_width=30;
+block_height=30;
+
+var player="";
+var block="";
+
+function player_Update(){
+    fabric.Image.fromURL("player.png",function(Img){
+    player=Img;
+    player.scaleToWidth(150);
+    player.scaleToHeight(140);
+    player.set({
+        top:player_y,
+        left:player_x
+    });
+    canvas.add(player);
+    });
+
+}
+function image(get_image){
+    fabric.Image.fromURL(get_image,function(Img){
+        block=Img;
+        block.scaleToWidth(block_width);
+        block.scaleToHeight(block_height);
+        block.set({
+        top:player_y,
+        left:player_x
+        });
+        canvas.add(block);
+    });
+}
